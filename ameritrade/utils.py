04405c18848ba8b70b7592df17f5e3e3ff674b55
Martin Blais <blais@furius.ca>
2021-01-29 01:10:08 -0500

    utils: Added method to get positions.

--- ameritrade/utils.py
+++ ameritrade/utils.py
@@ -4,12 +4,10 @@ __author__ = 'Martin Blais <blais@furius.ca>'
 __license__ = "GNU GPLv2"
 
 
-from typing import Optional
+from typing import Any, Optional
 
 import ameritrade as td
 
-
-import pprint
 
 def GetMainAccount(api: td.AmeritradeAPI, acctype: Optional[str]=None) -> str:
     """Returns the largest account of a particular type."""
@@ -23,6 +21,13 @@ def GetMainAccount(api: td.AmeritradeAPI, acctype: Optional[str]=None) -> str:
     if not matching_accounts:
         raise ValueError("No matching accounts.")
     return next(iter(sorted(matching_accounts, reverse=True)))[1]
+
+
+def GetPositions(api: td.AmeritradeAPI, account_id: str) -> Any:
+    """Fetch the account if of the main account. Return JSON."""
+    account = api.GetAccount(accountId=account_id, fields='positions')
+    acc = next(iter(account.items()))[1]
+    return acc['positions']
 
 
 ACTIVE_STATUS = frozenset({
