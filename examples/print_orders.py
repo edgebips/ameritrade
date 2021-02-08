dfe68ecc578b7afb14bf4882fb3a0e3ee8a8fd93
Martin Blais <blais@furius.ca>
2021-02-08 08:42:54 -0500

    Updated print orders.

--- examples/print_orders.py
+++ examples/print_orders.py
@@ -1,32 +1,20 @@
 #!/usr/bin/env python3
-"""Print active orders (for debugging).
+"""Print all active orders (for debugging).
 """
 __author__ = 'Martin Blais <blais@furius.ca>'
 
 import argparse
-import collections
-import datetime
-import logging
-import math
-import itertools
 import pprint
-import re
-import time
-from typing import Any, Optional, Tuple
 
 import ameritrade as td
 from ameritrade import utils
 
 
 def main():
-    """Compute various vol estimates."""
-    logging.basicConfig(level=logging.INFO, format='%(levelname)-8s: %(message)s')
     parser = argparse.ArgumentParser(description=__doc__.strip())
     td.add_args(parser)
     args = parser.parse_args()
-    config = td.config_from_args(args)
-    config = config._replace(readonly=False)
-    api = td.open(config)
+    api = td.open(td.config_from_args(args))
     account_id = utils.GetMainAccount(api)
 
     orders = api.GetOrdersByPath(accountId=account_id)
