dd676d686e9c8e34feee07bddaa346ad3686602d
blais <blais@furius.ca>
2022-01-10 00:43:53 -0500

    Added throttling to the API.

--- ameritrade/api.py
+++ ameritrade/api.py
@@ -1,12 +1,13 @@
 """Main API code for the Ameritrade API wrappers.
 """
-__author__ = 'Martin Blais <blais@furius.ca>'
+__author__ = "Martin Blais <blais@furius.ca>"
 __license__ = "GNU GPLv2"
 
 from decimal import Decimal
 from os import path
-from typing import Tuple, Dict, Optional, NamedTuple,Optional
+from typing import Tuple, Dict, Optional, NamedTuple, Optional
 import builtins
+import collections
 import hashlib
 import logging
 import os
@@ -27,57 +28,55 @@ from ameritrade import schema
 
 
 DEFAULT_CONFIG_DIR = os.environ.get(
-    'AMERITRADE_DIR', path.join(os.getenv('HOME'), '.ameritrade'))
+    "AMERITRADE_DIR", path.join(os.getenv("HOME"), ".ameritrade")
+)
 
 
-_Config = NamedTuple('_Config', [
-    # The OAuth client id string, such as '<USERNAME>@AMER.OAUTHAP'.
-    ('client_id', str),
-
-    # A redirect URI, such as 'https://127.0.0.1:8444'. Note that this must
-    # match that with which you created your app in the Ameritrade API
-    # interface.
-    ('redirect_uri', str),
-
-    # The location of the key PEM file.
-    ('key_file', str),
-
-    # The location of the certificate PEM file.
-    ('certificate_file', str),
-
-    # The location of the JSON file to store the OAuth token in between
-    # invocations.
-    ('secrets_file', str),
-
-    # Timeout (in seconds) to wait for OAuth token response.
-    ('timeout', int),
-
-    # Safe-mode that disallows any methods that modify state. Only allows
-    # getters to read data from the account.
-    ('readonly', bool),
-
-    # Cache directory; if this is set, requests matching a historical request
-    # will be fetched from the cache instead of being sent to the server
-    # upstream. This is an easy way to avoid hitting the servers too often
-    # during development and after the cache is filled, returns instantly. By
-    # default this is not set, so all requests are sent to the server and you
-    # don't need this otherwise.
-    ('cache_dir', Optional[str]),
-
-    # Authenticate or refresh secrets lazily, upon first attribute access.
-    ('lazy', bool),
-
-    # Enable debug traces.
-    ('debug', bool),
-    ])
+_Config = NamedTuple(
+    "_Config",
+    [
+        # The OAuth client id string, such as '<USERNAME>@AMER.OAUTHAP'.
+        ("client_id", str),
+        # A redirect URI, such as 'https://127.0.0.1:8444'. Note that this must
+        # match that with which you created your app in the Ameritrade API
+        # interface.
+        ("redirect_uri", str),
+        # The location of the key PEM file.
+        ("key_file", str),
+        # The location of the certificate PEM file.
+        ("certificate_file", str),
+        # The location of the JSON file to store the OAuth token in between
+        # invocations.
+        ("secrets_file", str),
+        # Timeout (in seconds) to wait for OAuth token response.
+        ("timeout", int),
+        # Max transactions per minute (for throttling).
+        ("rate_per_minute", Optional[int]),
+        # Safe-mode that disallows any methods that modify state. Only allows
+        # getters to read data from the account.
+        ("readonly", bool),
+        # Cache directory; if this is set, requests matching a historical request
+        # will be fetched from the cache instead of being sent to the server
+        # upstream. This is an easy way to avoid hitting the servers too often
+        # during development and after the cache is filled, returns instantly. By
+        # default this is not set, so all requests are sent to the server and you
+        # don't need this otherwise.
+        ("cache_dir", Optional[str]),
+        # Authenticate or refresh secrets lazily, upon first attribute access.
+        ("lazy", bool),
+        # Enable debug traces.
+        ("debug", bool),
+    ],
+)
 
 
 _ConfigDefaults = {
-    'redirect_uri': auth.DEFAULT_REDIRECT_URI,
-    'timeout': 300,
-    'readonly': True,
-    'lazy': False,
-    'debug': False,
+    "redirect_uri": auth.DEFAULT_REDIRECT_URI,
+    "timeout": 300,
+    "rate_per_minute": 120,
+    "readonly": True,
+    "lazy": False,
+    "debug": False,
 }
 
 
@@ -87,10 +86,12 @@ class Config(_Config):
     def __new__(cls, *args, **kwargs):
         """Create a Config instance using the kwargs."""
         cpargs = dict(kwargs)
-        args += tuple(cpargs.pop(key, _ConfigDefaults.get(key, None))
-                      for key in Config._fields[len(args):])
+        args += tuple(
+            cpargs.pop(key, _ConfigDefaults.get(key, None))
+            for key in Config._fields[len(args) :]
+        )
         if cpargs:
-            raise ValueError("Invalid arguments: {}".format(','.join(cpargs.keys())))
+            raise ValueError("Invalid arguments: {}".format(",".join(cpargs.keys())))
         return _Config.__new__(cls, *args)
 
 
@@ -103,18 +104,18 @@ def config_from_dir(config_dir: Optional[str] = None, **kwargs) -> Config:
         config_dir = DEFAULT_CONFIG_DIR
 
     # Set filenames from dir if not set.
-    if newargs.get('key_file', None) is None:
-        newargs['key_file'] = path.join(config_dir, 'key.pem')
-    if newargs.get('certificate_file', None) is None:
-        newargs['certificate_file'] = path.join(config_dir, 'certificate.pem')
-    if newargs.get('secrets_file', None) is None:
-        newargs['secrets_file'] = path.join(config_dir, 'secrets.json')
+    if newargs.get("key_file", None) is None:
+        newargs["key_file"] = path.join(config_dir, "key.pem")
+    if newargs.get("certificate_file", None) is None:
+        newargs["certificate_file"] = path.join(config_dir, "certificate.pem")
+    if newargs.get("secrets_file", None) is None:
+        newargs["secrets_file"] = path.join(config_dir, "secrets.json")
 
     # Read the client id from the 'config/client_id' file.
-    if newargs.get('client_id', None) is None:
-        client_id_file = path.join(config_dir, 'client_id')
+    if newargs.get("client_id", None) is None:
+        client_id_file = path.join(config_dir, "client_id")
         with builtins.open(client_id_file) as clifile:
-            newargs['client_id'] = clifile.read().strip()
+            newargs["client_id"] = clifile.read().strip()
 
     return Config(**newargs)
 
@@ -145,6 +146,7 @@ class AmeritradeAPI:
         self.secrets = None
         if not config.lazy:
             self.get_secrets()
+        self.time_queue = collections.deque()
 
     def get_secrets(self):
         if self.secrets is None:
@@ -163,12 +165,14 @@ class AmeritradeAPI:
         config = self.config
 
         # Disallow read-only methods.
-        if config.readonly and method.http_method != 'GET':
-            raise NameError("Method {} is not allowed in read-only mode.".format(
-                method.name))
+        if config.readonly and method.http_method != "GET":
+            raise NameError(
+                "Method {} is not allowed in read-only mode.".format(method.name)
+            )
         else:
             # Create a method, with caching or not.
-            method = CallableMethod(method, self, config.debug)
+            method = CallableMethod(method, self, config.debug,
+                                    self.time_queue, config.rate_per_minute)
             if config.cache_dir:
                 method = CachedMethod(config.cache_dir, key, method, config.debug)
             return method
@@ -179,16 +183,61 @@ def open(config: Config) -> AmeritradeAPI:
     return AmeritradeAPI(config)
 
 
+# An extra buffer of time to ensure we don't go over the threshold.
+BUFFER = 3  # secs
+
+
+def maybe_throttle(time_queue: collections.deque, rate_per_minute: int):
+    now = time.time()
+
+    if time_queue:
+        first = time_queue[0]
+        last = time_queue[-1]
+    else:
+        first = last = None
+
+    # Clear out timestamps before the past minute.
+    start_t = now - 60 + BUFFER # secs
+    while time_queue:
+        t = time_queue.popleft()
+        if t > start_t:
+            time_queue.appendleft(t)
+            break
+
+    # Sleep until the next item if we have to.
+    if len(time_queue) >= rate_per_minute:
+        t = time_queue.popleft()
+        dt = t - start_t
+        logging.info(f"Throttling for {dt:.1f} secs ({start_t} {t})")
+        time.sleep(dt)
+
+    now = time.time()
+    time_queue.append(now)
+
+
 class CallableMethod:
     """Callable method."""
 
-    def __init__(self, method: schema.PreparedMethod, api: AmeritradeAPI, debug: bool):
+    def __init__(
+        self,
+        method: schema.PreparedMethod,
+        api: AmeritradeAPI,
+        debug: bool,
+        time_queue: Optional[collections.deque],
+        rate_per_minute: Optional[int],
+    ):
         self.method = method
         self.api = api
+        self.rate_per_minute = rate_per_minute
+        self.time_queue = time_queue
         self.debug = debug
 
     def __call__(self, **kw):
         method = self.method
+
+        # Apply throttling.
+        if self.rate_per_minute:
+            maybe_throttle(self.time_queue, self.rate_per_minute)
 
         # Remove values which are None.
         kw = {key: value for key, value in kw.items() if value is not None}
@@ -219,46 +268,47 @@ class CallableMethod:
         # Build the headers and URL path to call.
         path_kw = {field: kw.pop(field) for field in method.url_fields}
         path = method.path.format(**path_kw)
-        url = 'https://api.tdameritrade.com/v1{}'.format(path)
+        url = "https://api.tdameritrade.com/v1{}".format(path)
         logging.info("Opening URL: %s", url)
 
         # Call the resources with parameters.
         # TODO(blais): Clean this up with methods.
         params = {key: str(value) for key, value in kw.items()}
         extra_headers = {}
-        if self.method.http_method == 'GET':
+        if self.method.http_method == "GET":
             # Those methods have query params (something none of them), never a
             # payload, but always a JSON response.
             logging.debug("With params: %s", params)
             call = lambda hdrs: requests.get(url, params=params, headers=hdrs)
             retvalue = lambda r: r.json(**JSON_KWARGS) if r.text else None
 
-        elif self.method.http_method == 'DELETE':
+        elif self.method.http_method == "DELETE":
             # Those methods only have the URL, no query params nor payload.
             # Never a response body.
             call = lambda hdrs: requests.delete(url, params=params, headers=hdrs)
             retvalue = lambda r: r.json(**JSON_KWARGS) if r.text else None
 
-        elif self.method.http_method in {'POST', 'PUT', 'PATCH'}:
+        elif self.method.http_method in {"POST", "PUT", "PATCH"}:
             # These methods never have query params but all have a payload.
             # Never a response body.
-            logging.debug("With payload: %s", kw['payload'])
-            extra_headers['Content-Type'] = 'application/json'
+            logging.debug("With payload: %s", kw["payload"])
+            extra_headers["Content-Type"] = "application/json"
             method = getattr(requests, self.method.http_method.lower())
-            call = lambda hdrs: method(url, json=kw['payload'], headers=hdrs)
+            call = lambda hdrs: method(url, json=kw["payload"], headers=hdrs)
             retvalue = lambda r: r.json(**JSON_KWARGS) if r.text else None
 
         else:
             # TODO(blais): Implement the other methods along with their schemas.
-            assert False, "Unsupported HTTP method for {}: {}".format(method.name,
-                                                                      method.http_method)
+            assert False, "Unsupported HTTP method for {}: {}".format(
+                method.name, method.http_method
+            )
 
         # Make the first attempt to call the method.
         secrets = self.api.get_secrets()
         headers = auth.get_headers(secrets)
         headers.update(extra_headers)
         resp = call(headers)
-        if resp.status_code == requests.codes['unauthorized']:  # HTTP error 401
+        if resp.status_code == requests.codes["unauthorized"]:  # HTTP error 401
             # If the token is expired, refresh the token automatically and retry
             # once, maybe even a few times, if we need some resilience for a job
             # that needs to run all week (e.g., a monitoring job).
@@ -272,12 +322,14 @@ class CallableMethod:
                 time.sleep(0.3)  # TODO(blais): Add configuration.
             else:
                 # Oh well, still failed. Bail out.
-                raise IOError("HTTP Error {}: {} ({})".format(
-                    resp.status_code, resp.reason, resp.text))
+                raise IOError(
+                    "HTTP Error {}: {} ({})".format(
+                        resp.status_code, resp.reason, resp.text
+                    )
+                )
 
         # Return either JSON or text, depending on method.
         return retvalue(resp)
-
 
 
 class CachedMethod:
@@ -309,14 +361,16 @@ class CachedMethod:
         if path.exists(cache_path):
             # Cache hit.
             logging.info("{%s} Cache hit for call to %s", digest, self.method_name)
-            with builtins.open(cache_path, 'r') as infile:
+            with builtins.open(cache_path, "r") as infile:
                 return json.load(infile, **JSON_KWARGS)
         else:
             # Cache miss.
             logging.info("{%s} Cache miss for call to %s", digest, self.method_name)
             response = self.method(**kw)
             logging.info("{%s} Updating cache for call to %s", digest, self.method_name)
-            if 'error' not in response:
-                with builtins.open(cache_path, 'w') as infile:
-                    json.dump(response, infile, sort_keys=True, indent=4, use_decimal=True)
+            if "error" not in response:
+                with builtins.open(cache_path, "w") as infile:
+                    json.dump(
+                        response, infile, sort_keys=True, indent=4, use_decimal=True
+                    )
             return response
